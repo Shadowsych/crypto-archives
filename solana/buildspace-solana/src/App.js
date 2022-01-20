6afa1682dc8cf3ceafe6942734e0b2e5a89026dc
Pravat Bhusal <pravat.bhusal@gmail.com>
2022-01-19 19:25:38 -0600

    Program section 1 of the buildspace project

--- solana/buildspace-solana/src/App.js
+++ solana/buildspace-solana/src/App.js
@@ -1,11 +1,59 @@
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from 'react';
+import { checkIfWalletIsConnected, connectWallet, sendGif } from './utils/wallet-utils';
+import { TEST_GIFS } from './constants/tests';
 import './App.css';
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState();
+  const [inputValue, setInputValue] = useState('');
+
+  // Verify that a Solana wallet is already connected once this component mounts
+  useEffect(() => {
+    const onLoad = async () => {
+      const publicKey = await checkIfWalletIsConnected();
+      setWalletAddress(publicKey);
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  const onConnectWallet = async () => {
+    const publicKey = await connectWallet();
+    setWalletAddress(publicKey);
+  }
+
+  // Renders a button for the app to connect to the wallet if it's not already connected
+  const renderConnectToWallet = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={onConnectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const onSubmitGifForm = async (event) => {
+    event.preventDefault();
+    await sendGif(inputValue);
+  }
+
+  // Renders the GIFs if the wallet is connected to the app
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form onSubmit={onSubmitGifForm}>
+        <input type="text" placeholder="Enter gif link!" value={inputValue} onChange={event => setInputValue(event.target.value)} />
+        <button type="submit" className="cta-button submit-gif-button">Submit</button>
+      </form>
+      <div className="gif-grid">
+        {TEST_GIFS.map(gif => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
       <div className="container">
@@ -14,17 +62,11 @@ const App = () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          {!walletAddress && renderConnectToWallet()}
+          {walletAddress && renderConnectedContainer()}
         </div>
       </div>
+      <div className="footer-container" />
     </div>
   );
 };
---
+++ solana/buildspace-solana/src/utils/wallet-utils.js
@@ -0,0 +1,60 @@
+/**
+ * Verifies if the "solana" object exists in the window, which means that a wallet extension is installed on the
+ * user's browser. Then, it will verify if the wallet is already connected to the app.
+ * 
+ * @returns {Promise<string>} public key of the wallet
+ */
+export const checkIfWalletIsConnected = async () => {
+  try {
+    const { solana } = window;
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('The user has a Phantom wallet!');
+
+        /**
+         * This code belows confirms if the app is authorized to access the user's wallet. If not, it will throw an error.
+         * 
+         * The onlyIfTrusted parameter will automatically pull the wallet's data without prompting them with another
+         * connect popup if the user has already connected their wallet to the app before.
+         */
+        const response = await solana.connect({ onlyIfTrusted: true });
+        const publicKey = response.publicKey.toString();
+        console.log('Connected with Public Key:', publicKey);
+        return publicKey;
+      }
+    } else {
+      alert('Could not find the Solana object in the Javascript window, please install the Phantom Wallet!');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Connects the app to the Solana wallet.
+ * 
+ * @returns {Promise<string>} public key of the wallet
+ */
+export const connectWallet = async () => {
+  const { solana } = window;
+  if (solana) {
+    const response = await solana.connect();
+    const publicKey = response.publicKey.toString();
+    console.log('Connected with Public Key:', publicKey);
+    return publicKey;
+  }
+}
+
+/**
+ * Sends the GIF data to Solana.
+ * 
+ * @param {string} url the URL to the GIF
+ * @returns
+ */
+export const sendGif = async (url) => {
+  if (url.length > 0) {
+    console.log('Gif link:', url);
+  } else {
+    console.log('Empty input. Try again.');
+  }
+};
